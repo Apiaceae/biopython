ec57ddc4ffff2cd74e3bda6572adcca07cd392b9
Lisong Wang <wangls76@gmail.com>
2015-11-07 11:13:16 +0000

    refine statistics for each genus+sp+population

--- statistic_sample_matrix.py
+++ statistic_sample_matrix.py
@@ -11,7 +11,7 @@ from Bio import SeqIO
 userParameters=sys.argv[1:]
 
 f = open(userParameters[1], "w")
-f.write("TaxonList"+"\n")
+#f.write("TaxonList"+"\n")
 
 for seq in SeqIO.parse(userParameters[0], "fasta"):
   taxon = str(seq.id).split("_")
@@ -38,12 +38,24 @@ for line in open(userParameters[1]):
   taxon_name_str = str(line).split(" ")
   genus_name.append(taxon_name_str[0])
 
-fa.write("############################################"+"\n")
-fa.write("Your DNA sequence matrix include "+ str(len(set(genus_name))) + " genera, and " + str(sample_total) + " species, there are total " + str(sample_total) + " samples" + "\n")
-fa.write("############################################"+"\n")
+fa.write("################################################################################################################################################################################"+"\n")
+fa.write("Your DNA sequence matrix include "+ str(len(set(genus_name))) + " genera, and " + str(len(set(taxon_name))) + " species, there are total " + str(sample_total) + " samples" + "\n")
+fa.write("################################################################################################################################################################################"+"\n"+"\n")
 
-fa.write("########Population sample number for each taxon#########"+"\n")
+fa.write("####################################################################"+"\n")
+fa.write("Population sample number for each taxon ordered by taxon name"+"\n")
+fa.write("####################################################################"+"\n")
+#taxon_name = taxon_name.remove("TaxonList"+"\n")
 
+from collections import Counter
+population_sample_number = Counter(taxon_name)
+for x in sorted(population_sample_number.items()):
+  #print x[0], x[1]
+  fa.write(x[0].rstrip() + " "+ str(x[1])+"\n")
+
+fa.write("####################################################################"+"\n")
+fa.write("Population sample number for each taxon ordered sample number"+"\n")
+fa.write("####################################################################"+"\n")
 #taxon_name = taxon_name.remove("TaxonList"+"\n")
 
 from collections import Counter
@@ -52,16 +64,53 @@ for x in population_sample_number:
   #print x[0], x[1]
   fa.write(x[0].rstrip() + " "+ str(x[1])+"\n")
 
-fa.write("######## Species sample number for each genus #########"+"\n")
+fa.write("####################################################################"+"\n")
+fa.write("Population sample number for each genus ordered by genus name"+"\n")
+fa.write("####################################################################"+"\n")
 
-species_sample_number = Counter(genus_name).most_common()
-for y in species_sample_number:
+species_sample_number = Counter(genus_name)
+for y in sorted(species_sample_number.items()):
   #print y[0], y[1]
   fa.write(y[0] + " "+ str(y[1])+"\n")
 
+fa.write("###################################################################################################"+"\n")
+fa.write("Species and population [species, population] sample number under each genus ordered by genus name"+"\n")
+fa.write("###################################################################################################"+"\n")
+
+
+taxon_sample_list = dict(Counter(taxon_name))
+taxon_name_list = sorted(taxon_sample_list.keys())
+
+genus_name_a=[]
+for x in taxon_name_list:
+  taxon_name_a = str(x).split(" ")
+  genus_name_a.append(str(taxon_name_a[0]))
+
+from collections import defaultdict
+
+counter = dict(sorted(Counter(genus_name_a).items()))
+# for x in counter:
+#   fa.write(x[0].rstrip() + " "+ str(x[1])+"\n")
+
+
+species_sample_number = dict(sorted(Counter(genus_name).items()))
+# for y in sorted(species_sample_number.items()):
+#   for x in counter:
+#   fa.write(x[0].rstrip() + " "+ str(x[1])+"\n")
+#   fa.write(y[0] + " "+ str(y[1])+"\n")
+dd = defaultdict(list)
+for d in (counter, species_sample_number):
+  for key, value in d.iteritems():
+      dd[key].append(value)
+
+# print sorted(dd.items())
+
+for x in sorted(dd.items()):
+  fa.write(x[0].rstrip() + " "+ str(x[1])+"\n")
+
 fa.close()
 
-print "=> Your DNA sequence data matrix statistcs finished, please check your file "+userParameters[2]
+print "=> &&& Your DNA sequence data matrix statistcs finished, please check your file => "+userParameters[2]
 # print genus_name
 # print taxon_name
 # print species_sample_number
